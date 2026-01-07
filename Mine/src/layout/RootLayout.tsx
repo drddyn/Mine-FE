d7a764c221610c34790f348b4b3fbda7710f5e6e
yoonvinjeong <anaabc1004@naver.com>
2026-01-07 12:00:06 +0900

    feat: 사이드바 라우팅 처리

--- Mine/src/layout/RootLayout.tsx
+++ Mine/src/layout/RootLayout.tsx
@@ -1,10 +1,15 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Sidebar from './Sidebar'
 
 export default function RootLayout() {
+    const location = useLocation()
+
+    const hideSidebarRoutes = ['/login', '/signup']
+    const shouldHideSidebar = hideSidebarRoutes.includes(location.pathname)
+
     return (
         <div className="relative">
-            <Sidebar />
+            {!shouldHideSidebar && <Sidebar />}
             <Outlet />
         </div>
     )
