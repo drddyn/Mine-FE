de572b768c3c896a6e6b31cb8a77508bfab54fa9
yoonvinjeong <anaabc1004@naver.com>
2026-01-11 14:31:12 +0900

    feat: 매거진 UI 작업

---
+++ Mine/src/pages/magazine/components/MagazineInfo.tsx
@@ -0,0 +1,11 @@
+export default function MagazineInfo() {
+    return (
+        <div className="flex max-w-245 w-full justify-between items-center self-center mt-9">
+            <div className="font-semibold24 font-poltawski text-black-textBigTitle">LAW</div>
+            <div className="flex items-center gap-2">
+                <div className="font-regular16 text-black-textMain">닉네임</div>
+                <div className="w-12.5 h-12.5 rounded-full bg-black-icon"></div>
+            </div>
+        </div>
+    )
+}
---
+++ Mine/src/pages/magazine/components/SectionPart.tsx
@@ -0,0 +1,25 @@
+interface SectionPartProps {
+    hint?: string
+    size?: string
+}
+
+export default function SectionPart({ hint, size }: SectionPartProps) {
+    return (
+        <section className="group flex w-full h-69.5 items-start gap-10" dir={hint}>
+            <div className={`w-57.5 h-full bg-black-image rounded-lg ${size}`}></div>
+            <div className="flex flex-col ltr:ml-2.5 rtl:mr-2.5 h-full items-start gap-6 flex-1">
+                <div className="font-semibold36 text-black-textBigTitle">본문 제목</div>
+                <div className="w-full font-regular16 text-black-textMain break-all overflow-hidden" dir="ltr">
+                    1줄 본문 내용은 width = fill, height = hug 본문 내용은 width = fill, height = hug 본문 내용은 width
+                    = fill, 가나다라마바사아자카타파하 가나다라마바사아자카타파하 가나다라마바사아자카타파하
+                    가나다라마바사아자카타파하 가나다라마바사아자카타파하 가나다라마바사아자카타파하
+                    가나다라마바사아자카타파하 가나다라마바사아자카타파하 가나다라마바사아자카타파하
+                    가나다라마바사아자카타파하 가나다라마바사아자카타파하 가나다라마바사아자카타파하 법률안에 이의가
+                    있을 때에는 대통령은 제1항의 기간내에 이의서를 붙여 국회로 환부하고, 그 재의를 요구할 수 있다.
+                    국회의 폐회중에도 또한 같다. 선거와 국민투표의 공정한 관리 및 정당에 관한 사무를 처리하기 위하여 ...
+                    이미지 크기는 고정(fix), 텍스트는 유동적(hug)! 7줄
+                </div>
+            </div>
+        </section>
+    )
+}
