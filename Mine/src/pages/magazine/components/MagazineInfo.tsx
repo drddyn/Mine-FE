0fd031f60946737cb2a09ca15254b90f03703aad
yoonvinjeong <anaabc1004@naver.com>
2026-01-11 15:43:15 +0900

    feat: 매거진 상단 정보 수정
    - 마루부리 글꼴 추가

--- Mine/src/pages/magazine/components/MagazineInfo.tsx
+++ Mine/src/pages/magazine/components/MagazineInfo.tsx
@@ -1,11 +1,9 @@
+import Share from '../../../icon/share.svg?react'
 export default function MagazineInfo() {
     return (
-        <div className="flex max-w-245 w-full justify-between items-center self-center mt-9">
-            <div className="font-semibold24 font-poltawski text-black-textBigTitle">LAW</div>
-            <div className="flex items-center gap-2">
-                <div className="font-regular16 text-black-textMain">닉네임</div>
-                <div className="w-12.5 h-12.5 rounded-full bg-black-icon"></div>
-            </div>
+        <div className="flex max-w-245 w-full justify-between items-center self-stretch mt-9">
+            <div className="font-regular16 font-maruburi text-black-textBigTitle">2025 브랜드 리브랜딩</div>
+            <Share />
         </div>
     )
 }
