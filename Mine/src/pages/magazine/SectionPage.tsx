07b713c99fdca1c918f54070a7e1f686f38e4054
yoonvinjeong <anaabc1004@naver.com>
2026-01-12 21:02:32 +0900

    style: w-screen 속성 제거

--- Mine/src/pages/magazine/SectionPage.tsx
+++ Mine/src/pages/magazine/SectionPage.tsx
@@ -3,14 +3,14 @@ import SectionPart from './components/SectionPart'
 
 export default function SectionPage() {
     return (
-        <div className="flex bg-black-image w-screen  justify-center">
+        <div className="flex bg-black-image   justify-center">
             <div className="flex justify-center w-275 h-full bg-white">
                 <div className="flex flex-col w-245 items-center gap-14 mb-60">
                     <MagazineInfo />
-                    <SectionPart hint="rtl" />
+                    <SectionPart sectionDir="rtl" titleDir="ltr" />
                     <SectionPart />
-                    <SectionPart size="w-[340px] h-[338px]" />
-                    <SectionPart hint="rtl" />
+                    <SectionPart size="w-[340px] h-[338px]" titleDir="rtl" />
+                    <SectionPart sectionDir="rtl" />
                 </div>
             </div>
         </div>
