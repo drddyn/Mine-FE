82804bddf9a6eafe0da0a17d2d5c216148e10dfc
yoonvinjeong <anaabc1004@naver.com>
2026-01-20 00:09:08 +0900

    style: 세팅페이지 스타일링 수정

--- Mine/src/components/settings/ScreenSettings.tsx
+++ Mine/src/components/settings/ScreenSettings.tsx
@@ -1,60 +1,18 @@
-import IconWandStars from "../../icon/wand_stars.svg?react";
-import IconAddPhoto from "../../icon/add_photo_alternate.svg?react";
+import IconWandStars from '../../icon/wand_stars.svg?react'
+import IconAddPhoto from '../../icon/add_photo_alternate.svg?react'
 
 export default function ScreenSettings() {
-  return (
-    <div className="w-136.5 h-36.5 flex gap-6.5">
-      <button
-        className="
-          w-65
-          h-full
-          rounded-2xl
-          border border-lightborder
-          bg-white
-          flex
-          flex-col
-          items-center
-          justify-center
-          gap-3
-          text-black-whiteBoxOutline
-          transition-colors
-          duration-150
-          hover:bg-buttonbackgd
-          hover:border-boldborder
-          hover:text-lightborder
-        "
-      >
-        <IconWandStars className="w-6 h-6 aspect-square **:stroke-current **:fill-current" />
-        <span className="font-regular12">
-          AI로 무드보드 생성하기
-        </span>
-      </button>
+    return (
+        <div className="w-136.5 h-36.5 flex gap-6.5">
+            <button className="flex flex-col w-65 h-full rounded-2xl border border-lightborder bg-white items-center justify-center gap-3 text-black-whiteBoxOutline transition-colors duration-150 hover:bg-buttonbackgd hover:border-boldborder hover:text-lightborder">
+                <IconWandStars className="w-6 h-6 aspect-square **:stroke-current **:fill-current" />
+                <span className="font-regular14">AI로 무드보드 생성하기</span>
+            </button>
 
-      <button
-        className="
-          w-65
-          h-full
-          rounded-2xl
-          border border-lightborder
-          bg-white
-          flex
-          flex-col
-          items-center
-          justify-center
-          gap-3
-          text-black-whiteBoxOutline
-          transition-colors
-          duration-150
-          hover:bg-buttonbackgd
-          hover:border-boldborder
-          hover:text-lightborder
-        "
-      >
-        <IconAddPhoto className="w-6 h-6 aspect-square **:stroke-current **:fill-current" />
-        <span className="font-regular12">
-          컴퓨터에서 이미지 가져오기
-        </span>
-      </button>
-    </div>
-  );
+            <button className="flex flex-col w-65 h-full rounded-2xl border border-lightborder bg-white items-center justify-center gap-3 text-black-whiteBoxOutline transition-colors duration-150 hover:bg-buttonbackgd hover:border-boldborder hover:text-lightborder ">
+                <IconAddPhoto className="w-6 h-6 aspect-square **:stroke-current **:fill-current" />
+                <span className="font-regular14">컴퓨터에서 이미지 가져오기</span>
+            </button>
+        </div>
+    )
 }
--- Mine/src/layout/sidebar/SidebarOpen.tsx
+++ Mine/src/layout/sidebar/SidebarOpen.tsx
@@ -9,39 +9,48 @@ import Sidebar_others from '../../icon/sidebar_others.svg?react'
 import SidebarMagazine from './SidebarMagazine'
 import SidebarSectionList from './SidebarSectionList'
 import Setting from '../../icon/setting.svg?react'
+import { useState } from 'react'
+import SettingsModal from '../../components/settings/SettingsModal'
 
 interface SidebarProps {
     onclick: () => void
 }
 
 export default function SidebarOpen({ onclick }: SidebarProps) {
-    return (
-        <div className="h-screen w-66.5 bg-main-light rounded-r-[25px] z-50 transition-transform duration-30">
-            <div className="flex flex-col h-full gap-6">
-                <div className="flex gap-2 mt-11.5 ml-6.25 items-center">
-                    <MineLogo />
-                    <div className="pr-20 font-semibold24 text-main-default">MINE</div>
-                    <SidebarButton onClick={onclick} />
-                </div>
-                <div className="flex flex-col">
-                    <SidebarBlock icon={<Sidebar_main />} title="메인" to="/mymagazine" />
-                    <SidebarBlock icon={<Sidebar_new />} title="새 매거진" to="" />
-                    <SidebarBlock icon={<Sidebar_like />} title="저장한 매거진" to="" />
-                    <SidebarBlock icon={<Sidebar_others />} title="둘러보기" to="" />
-                </div>
-                <div className="flex flex-col mt-6">
-                    <SidebarMagazine title="베스트셀러" child={<SidebarSectionList />} />
-                    <SidebarMagazine title="2025 브랜드 리브랜딩" />
-                </div>
+    const [IsSettingOpen, SetIsSettingOpen] = useState(false)
 
-                <div className="flex justify-between mt-auto mb-6 pl-6.5 pr-6 items-center ">
-                    <div className="flex gap-2 items-center">
-                        <div className="w-7.5 h-7.5 rounded-full bg-black-icon"></div>
-                        <div className="font-light14 text-black-textSmallTitle">닉네임</div>
+    return (
+        <>
+            <div className="h-screen w-66.5 bg-main-light rounded-r-[25px] z-50 transition-transform duration-30">
+                <div className="flex flex-col h-full gap-6">
+                    <div className="flex gap-2 mt-11.5 ml-6.25 items-center">
+                        <MineLogo />
+                        <div className="pr-20 font-semibold24 text-main-default">MINE</div>
+                        <SidebarButton onClick={onclick} />
                     </div>
-                    <Setting />
+                    <div className="flex flex-col">
+                        <SidebarBlock icon={<Sidebar_main />} title="메인" to="/mymagazine" />
+                        <SidebarBlock icon={<Sidebar_new />} title="새 매거진" to="" />
+                        <SidebarBlock icon={<Sidebar_like />} title="저장한 매거진" to="" />
+                        <SidebarBlock icon={<Sidebar_others />} title="둘러보기" to="" />
+                    </div>
+                    <div className="flex flex-col mt-6">
+                        <SidebarMagazine title="베스트셀러" child={<SidebarSectionList />} />
+                        <SidebarMagazine title="2025 브랜드 리브랜딩" />
+                    </div>
+
+                    <div className="flex justify-between mt-auto mb-6 pl-6.5 pr-6 items-center ">
+                        <div className="flex gap-2 items-center">
+                            <div className="w-7.5 h-7.5 rounded-full bg-black-icon"></div>
+                            <div className="font-light14 text-black-textSmallTitle">닉네임</div>
+                        </div>
+                        <button onClick={() => SetIsSettingOpen(true)} className="cursor-pointer">
+                            <Setting />
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+            {IsSettingOpen && <SettingsModal onClose={() => SetIsSettingOpen(false)} />}
+        </>
     )
 }
