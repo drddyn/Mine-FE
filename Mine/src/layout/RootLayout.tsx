11229516297806c8f97a5712e58cb3c34bf2797d
yoonvinjeong <anaabc1004@naver.com>
2026-02-11 15:37:39 +0900

    feat: 사이드바 레이아웃 추가

--- Mine/src/layout/RootLayout.tsx
+++ Mine/src/layout/RootLayout.tsx
@@ -1,19 +1,11 @@
-import { Outlet, useLocation } from 'react-router-dom'
-import Sidebar from './Sidebar'
+import { Outlet } from 'react-router-dom'
+
+import Layout from './Layout'
 
 export default function RootLayout() {
-    const location = useLocation()
-
-    const hideSidebarRoutes = ['/login', '/signup', '/landing']
-    const shouldHideSidebar = hideSidebarRoutes.includes(location.pathname)
-
     return (
-        <div className="relative min-h-screen">
-            {!shouldHideSidebar && <Sidebar />}
-
-            <div className={!shouldHideSidebar ? 'pl-20.5' : ''}>
-                <Outlet />
-            </div>
-        </div>
+        <Layout>
+            <Outlet />
+        </Layout>
     )
 }
