f24c61e053a31ba9e88fdb81a03f75af92c72432
yoonvinjeong <anaabc1004@naver.com>
2026-02-24 14:01:38 +0900

    feat:LLMInputBox 레이아웃 적용

---
+++ Mine/src/components/LLMInputLayout.tsx
@@ -0,0 +1,16 @@
+import { useLocation } from 'react-router-dom'
+import LLMInputBox from './LLMInputBox'
+
+export default function LLMInputLayout() {
+    const location = useLocation()
+    const hiddenPath = ['/login', '/signup', '/']
+    const isHiddenPath = hiddenPath.includes(location.pathname)
+
+    if (!isHiddenPath)
+        return (
+            <div className="absolute bottom-8 translate-x-1/2 justify-center">
+                <LLMInputBox />
+            </div>
+        )
+    return null
+}
