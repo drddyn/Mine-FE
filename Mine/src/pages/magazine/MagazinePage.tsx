b9fea3ec9c1caba5ae574dad66d13f4a27f7ba0b
eun_0903 <134464931+eun0903@users.noreply.github.com>
2026-03-06 13:16:27 +0900

    dev: 좋아요 매거진 페이지 API 연결

--- Mine/src/pages/magazine/MagazinePage.tsx
+++ Mine/src/pages/magazine/MagazinePage.tsx
@@ -12,18 +12,22 @@ export default function MagazinePage() {
     const handleSectionClick = (magazineId: number, sectionId: number) => {
         navigate(`/magazine/${magazineId}/section/${sectionId}`)
     }
+
     if (isPending) return <></>
+    if (!data) return <></>
 
     const content = data?.sections
     return (
         <div style={{ backgroundImage: `url(${data?.coverImageUrl})` }} className="bg-cover overflow-hidden">
             <div className="mt-9">
-                <ProfileBox
-                    nickname={data?.user.nickname}
-                    profileImage={data?.user.profileImageUrl}
-                    classname="justify-end mr-6.5"
-                    mode="magazine"
-                />
+                {data.user && (
+                    <ProfileBox
+                        nickname={data.user.nickname}
+                        profileImage={data.user.profileImageUrl}
+                        classname="justify-end mr-6.5"
+                        mode="magazine"
+                    />
+                )}
                 <div className="flex justify-center items-start p-30 h-screen overflow-auto">
                     <GridContainor>
                         {content?.map((item) => (
@@ -31,7 +35,7 @@ export default function MagazinePage() {
                                 key={item.sectionId}
                                 imageUrl={item.thumbnailUrl}
                                 onclick={() => handleSectionClick(Number(magazineId), item.sectionId)}
-                            /> // 작은 컴포넌트
+                            />
                         ))}
                     </GridContainor>
                 </div>
--- Mine/src/pages/magazine/components/SavedMagazineItem.tsx
+++ Mine/src/pages/magazine/components/SavedMagazineItem.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import type { Magazine } from '../../../types/magazine'
 
 type Props = {
@@ -5,8 +6,18 @@ type Props = {
 }
 
 export default function SavedMagazineItem({ magazine }: Props) {
+  const navigate = useNavigate()
+
   return (
-    <div className="flex w-119 h-67 p-[16px_20px] justify-end items-end gap-2.5 shrink-0 bg-black-textInTheBox">
+    <div
+      className="flex w-119 h-67 p-[16px_20px] justify-end items-end gap-2.5 shrink-0 cursor-pointer"
+      style={{
+        backgroundImage: `url(${magazine.coverImageUrl})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+      onClick={() => navigate(`/magazine/${magazine.magazineId}`)}
+    >
       <span className="text-white text-right leading-normal font-[MaruBuri] font-semibold24">
         {magazine.title}
       </span>
