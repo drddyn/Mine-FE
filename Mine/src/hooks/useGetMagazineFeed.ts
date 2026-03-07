316397c764cd6a3201bd6ae67775def35ed94229
eun_0903 <134464931+eun0903@users.noreply.github.com>
2026-03-07 13:23:28 +0900

    refactor: 타입 단언 제거

--- Mine/src/hooks/useGetMagazineFeed.ts
+++ Mine/src/hooks/useGetMagazineFeed.ts
@@ -3,9 +3,9 @@ import type { ResponseFeed } from '../types/magazine'
 import { getMagazineFeed } from '../api/magazine'
 
 export default function useGetMagazineFeed() {
-    return useInfiniteQuery<ResponseFeed>({
+    return useInfiniteQuery<ResponseFeed, Error, ResponseFeed, string[], number | null>({
         queryKey: ['magazineFeed'],
-        queryFn: ({ pageParam }) => getMagazineFeed({ cursorId: pageParam as number | null }),
+        queryFn: ({ pageParam }) => getMagazineFeed({ cursorId: pageParam }),
         initialPageParam: null,
         getNextPageParam: (lastPage) => lastPage.hasNext ? lastPage.nextCursor : undefined,
     })
