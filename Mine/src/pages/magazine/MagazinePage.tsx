9b24fb3634893034c8c64dccc1d715ccbbb211f7
yoonvinjeong <anaabc1004@naver.com>
2026-03-09 13:27:03 +0900

    feat: 매거진 대표 페이지 위 매거진 정보 추가

--- Mine/src/pages/magazine/MagazinePage.tsx
+++ Mine/src/pages/magazine/MagazinePage.tsx
@@ -4,6 +4,8 @@ import useGetMagazineDetail from '../../hooks/useGetMagazineDetail'
 import { GridContainor } from './components/GridContainor'
 import ProfileBox from './components/ProfileBox'
 import useSidebarStore from '../../stores/sidebar'
+import MagazineInfo from './components/MagazineInfo'
+import { MagazineProvider } from './MagazineProvider'
 
 const isValidUrl = (url?: string) => {
     if (!url) return false
@@ -32,31 +34,35 @@ export default function MagazinePage() {
     const content = data?.sections
 
     return (
-        <div
-            style={{ backgroundImage: safeCoverImageUrl ? `url(${safeCoverImageUrl})` : 'none' }}
-            className="bg-cover overflow-hidden"
-        >
-            <div className={`mt-9 transition-all duration-300 ${isOpen ? 'ml-60' : 'ml-15'}`}>
-                {data.user && (
-                    <ProfileBox
-                        nickname={data.user.nickname}
-                        profileImage={data.user.profileImageUrl}
-                        classname="justify-end mr-6.5"
-                        mode="magazine"
-                    />
-                )}
-                <div className="flex justify-center items-start p-20 h-screen overflow-auto">
-                    <GridContainor>
-                        {content?.map((item) => (
-                            <SectionCover
-                                key={item.sectionId}
-                                imageUrl={item.thumbnailUrl}
-                                onclick={() => handleSectionClick(Number(magazineId), item.sectionId)}
+        <MagazineProvider id={Number(magazineId)}>
+            <div
+                style={{ backgroundImage: safeCoverImageUrl ? `url(${safeCoverImageUrl})` : 'none' }}
+                className="bg-cover overflow-hidden"
+            >
+                <div className={`flex flex-col transition-all duration-300 ${isOpen ? 'ml-60' : 'ml-15'}`}>
+                    {data.user && (
+                        <div className="mt-9 pr-7 pl-10.5">
+                            <MagazineInfo
+                                nickname={data.user.nickname}
+                                profileImage={data.user.profileImageUrl}
+                                magazineId={data.magazineId}
+                                mode="magazine"
                             />
-                        ))}
-                    </GridContainor>
+                        </div>
+                    )}
+                    <div className="flex justify-center items-start p-20 h-screen overflow-auto">
+                        <GridContainor>
+                            {content?.map((item) => (
+                                <SectionCover
+                                    key={item.sectionId}
+                                    imageUrl={item.thumbnailUrl}
+                                    onclick={() => handleSectionClick(Number(magazineId), item.sectionId)}
+                                />
+                            ))}
+                        </GridContainor>
+                    </div>
                 </div>
             </div>
-        </div>
+        </MagazineProvider>
     )
 }
