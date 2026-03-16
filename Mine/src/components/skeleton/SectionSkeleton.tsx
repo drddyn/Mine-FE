58b60302dbdff7d824e4b4a67fa83c99faae789a
xxeeon <dudtjs@hanyang.ac,kr>
2026-03-16 20:59:06 +0900

    feat: 섹션 스켈레톤 컴포넌트 개선 및 아바타 추가

--- Mine/src/components/skeleton/SectionSkeleton.tsx
+++ Mine/src/components/skeleton/SectionSkeleton.tsx
@@ -1,27 +1,40 @@
-import { SkeletonBox, SkeletonTitle } from '../../components/skeleton/SkeletonBase'
+import { SkeletonBox, SkeletonTitle, SkeletonAvatar } from '../../components/skeleton/SkeletonBase'
 
 export default function SectionSkeleton() {
     return (
-        <div className="w-full flex flex-col">
-            {/* 상단 커버 이미지 영역 */}
-            <SkeletonBox width="w-full" height="h-64" rounded="rounded-none" />
+        <div className="w-full flex flex-col px-10 py-12 gap-16 max-w-[1000px] mx-auto">
             
-            <div className="w-full flex flex-col px-4 py-6 gap-6">
-                {/* 제목 및 메타 텍스트 영역 */}
-                <div className="flex flex-col gap-2">
-                    <SkeletonTitle width="w-3/4" height="h-8" />
-                    <SkeletonTitle width="w-1/3" height="h-4" />
+            {/* 1. 상단 헤더 영역 */}
+            <div className="flex w-full justify-between items-start">
+                <div className="flex flex-col gap-4 mt-2">
+                    <SkeletonTitle width="w-64" height="h-10" />
+                    <SkeletonTitle width="w-40" height="h-6" />
                 </div>
+                <div className="flex flex-col items-end gap-8">
+                    <SkeletonAvatar size="w-14 h-14" />
+                    <SkeletonBox width="w-8" height="h-8" rounded="rounded-md" />
+                </div>
+            </div>
 
-                {/* 내부 콘텐츠/아티클 목록 */}
-                <div className="flex flex-col gap-6 mt-4">
-                    {Array.from({ length: 3 }).map((_, idx) => (
-                        <div key={idx} className="flex flex-col gap-3">
-                            <SkeletonBox width="w-full" height="h-48" rounded="rounded-lg" />
-                            <SkeletonBox width="w-full" height="h-4" className="mt-2" />
-                            <SkeletonBox width="w-5/6" height="h-4" />
-                        </div>
-                    ))}
+            {/* 2. 본문 상단 - 텍스트 블록 (좌: 텍스트 여러 줄, 우: 세로 이미지) */}
+            <div className="flex w-full gap-10 items-start">
+                <div className="flex flex-col gap-5 flex-1 pt-4">
+                    <SkeletonTitle width="w-full" height="h-6" />
+                    <SkeletonTitle width="w-5/6" height="h-6" />
+                    <SkeletonTitle width="w-11/12" height="h-6" />
+                    <SkeletonTitle width="w-4/5" height="h-6" />
+                </div>
+                <SkeletonBox width="w-64 shrink-0" height="h-80" rounded="rounded-lg" />
+            </div>
+
+            {/* 3. 본문 하단 - 카드 2개 가로 배치 (좌: 정사각형 이미지, 우: 텍스트 여러 줄) */}
+            <div className="flex w-full gap-10 items-start">
+                <SkeletonBox width="w-64 shrink-0" height="h-64" rounded="rounded-lg" />
+                <div className="flex flex-col gap-5 flex-1 pt-4">
+                    <SkeletonTitle width="w-full" height="h-6" />
+                    <SkeletonTitle width="w-3/4" height="h-6" />
+                    <SkeletonTitle width="w-5/6" height="h-6" />
+                    <SkeletonTitle width="w-2/3" height="h-6" />
                 </div>
             </div>
         </div>
