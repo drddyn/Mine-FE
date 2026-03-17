a134122224733270f72ba5e29b8c01921df9a304
eun_0903 <134464931+eun0903@users.noreply.github.com>
2026-03-17 19:16:41 +0900

    dev: 매거진 페이지 햄버거 모달 연결 및 API 기능 추가

--- Mine/src/components/hamburgerModal/ParagraphHamburgerModal.tsx
+++ Mine/src/components/hamburgerModal/ParagraphHamburgerModal.tsx
@@ -1,7 +1,9 @@
+import { useRef } from 'react'
 import Edit from '../../icon/edit.svg?react'
 import Delete from '../../icon/delete.svg?react'
 import { HamburgerSection } from './HamburgerSection'
 import useDeleteParagraph from '../../hooks/useDeleteParagraph'
+import useClickOutside from '../../hooks/useClickOutside'
 
 interface ParagraphHamburgerModalProps {
     top: number
@@ -11,7 +13,6 @@ interface ParagraphHamburgerModalProps {
     paragraphId?: number
     children?: React.ReactNode
     handleClose: () => void
-    // onEdit: (id?: number) => void
 }
 
 export default function ParagraphHamburgerModal({
@@ -22,6 +23,9 @@ export default function ParagraphHamburgerModal({
     left,
     handleClose,
 }: ParagraphHamburgerModalProps) {
+    const modalRef = useRef<HTMLDivElement>(null)
+    useClickOutside(modalRef, handleClose)
+
     const deleteParagraphMutation = useDeleteParagraph()
     const onDeleteClick: React.MouseEventHandler<HTMLDivElement> = () => {
         deleteParagraphMutation.mutate({
@@ -34,8 +38,9 @@ export default function ParagraphHamburgerModal({
 
     return (
         <div
+            ref={modalRef}
             key={sectionId}
-            className="fixed flex flex-col px-1 py-1 rounded-lg bg-gray-500-op70  shadow-[0 4px 4px 0 rgba(0, 0, 0, 0.25)] z-100"
+            className="fixed flex flex-col px-1 py-1 rounded-lg bg-gray-500-op70 shadow-[0 4px 4px 0 rgba(0, 0, 0, 0.25)] z-100"
             style={{ top: `${top}px`, left: `${left}px` }}
         >
             <HamburgerSection title="이름 변경" icon={<Edit />} />
--- Mine/src/components/hamburgerModal/SidebarHamburgerModal.tsx
+++ Mine/src/components/hamburgerModal/SidebarHamburgerModal.tsx
@@ -1,8 +1,11 @@
+import { useState, useRef } from 'react'
 import Share from '../../icon/share.svg?react'
 import Edit from '../../icon/edit.svg?react'
 import Delete from '../../icon/delete.svg?react'
 import useDeleteMagazine from '../../hooks/useDeleteMagazine'
 import { HamburgerSection } from './HamburgerSection'
+import useClickOutside from '../../hooks/useClickOutside'
+import ShareModal from './ShareModal'
 
 interface SidebarHamburgerModalProps {
     top: number
@@ -13,7 +16,12 @@ interface SidebarHamburgerModalProps {
 }
 
 export default function SidebarHamburgerModal({ id, top, left, handleClose, onEdit }: SidebarHamburgerModalProps) {
+    const modalRef = useRef<HTMLDivElement>(null)
+    useClickOutside(modalRef, handleClose)
+
     const deleteMutation = useDeleteMagazine()
+    const [showShareModal, setShowShareModal] = useState(false)
+
     const onDeleteClick: React.MouseEventHandler<HTMLDivElement> = () => {
         deleteMutation.mutate({ id: id })
         handleClose()
@@ -22,15 +30,25 @@ export default function SidebarHamburgerModal({ id, top, left, handleClose, onEd
         onEdit(id)
         handleClose()
     }
+
     return (
-        <div
-            key={id}
-            className="fixed flex flex-col px-1 py-2 rounded-lg bg-gray-500-op70 shadow-[0 4px 4px 0 rgba(0, 0, 0, 0.25)] z-100"
-            style={{ top: `${top}px`, left: `${left}px` }}
-        >
-            <HamburgerSection title="공유" icon={<Share />} />
-            <HamburgerSection title="이름 변경" icon={<Edit />} onClick={onEditClick} />
-            <HamburgerSection title="삭제" icon={<Delete />} onClick={onDeleteClick} />
-        </div>
+        <>
+            {!showShareModal && (
+                <div
+                    ref={modalRef}
+                    key={id}
+                    className="fixed flex flex-col px-1 py-2 rounded-lg bg-gray-500-op70 shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] z-100"
+                    style={{ top: `${top}px`, left: `${left}px` }}
+                >
+                    <HamburgerSection title="공유" icon={<Share />} onClick={() => setShowShareModal(true)} />
+                    <HamburgerSection title="이름 변경" icon={<Edit />} onClick={onEditClick} />
+                    <HamburgerSection title="삭제" icon={<Delete />} onClick={onDeleteClick} />
+                </div>
+            )}
+
+            {showShareModal && (
+                <ShareModal onClose={handleClose} />
+            )}
+        </>
     )
 }
