42f918b80c80e89e3e6cf511fafa98dbbe9d2335
yoonvinjeong <anaabc1004@naver.com>
2026-03-30 21:49:30 +0900

    feat: 쿼리키 수정

--- Mine/src/pages/magazine/components/SectionContent.tsx
+++ Mine/src/pages/magazine/components/SectionContent.tsx
@@ -84,8 +84,8 @@ export default function SectionContent({ sectionId, magazineId }: SectionContent
                             profileImage={user?.profileImageUrl}
                             sectionId={Number(sectionId)}
                             magazineId={Number(magazineId)}
-                            likeCount={data?.likeCount} // data(섹션 상세정보)에서 가져온 하트수 전달
-                            isLiked={data?.isLiked} // 내 좋아요 상태 전달
+                            likeCount={magazinedata?.likeCount} // data(섹션 상세정보)에서 가져온 하트수 전달
+                            isLiked={magazinedata?.isLiked} // 내 좋아요 상태 전달
                             mode="section"
                             onClick={handleClick}
                         />
