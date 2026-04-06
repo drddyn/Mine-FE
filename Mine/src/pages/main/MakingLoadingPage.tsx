8a8095ee97ccd9a19a51caf617c0801e52b9900c
yoonvinjeong <anaabc1004@naver.com>
2026-04-06 15:54:53 +0900

    feat: 애니메이션 json파일 연결

--- Mine/src/pages/main/MakingLoadingPage.tsx
+++ Mine/src/pages/main/MakingLoadingPage.tsx
@@ -1,10 +1,13 @@
+import Lottie from 'lottie-react'
+import LoadingAnimation from '../../assets/mineLogoLoadingFinal1.json'
+
 export default function MakingLoadingPage() {
+    console.log(LoadingAnimation)
     return (
-        <div className="absolute inset-0 flex flex-col items-center justify-center transition-opacity duration-500 animate-fadeIn bg-gray-100/80 backdrop-blur-sm z-10">
-            <div className="w-10 h-10 border-4 border-gray-200 border-t-gray-500 rounded-full animate-spin mb-6" />
-            <p className="text-gray-600 animate-pulse font-notoserif font-semibold20">
-                당신만을 위한 매거진을 만들고 있어요...
-            </p>
+        <div className="fixed inset-0 flex items-center justify-center z-100 bg-gray-200">
+            <div className="w-30 h-40 lottie-container">
+                <Lottie animationData={LoadingAnimation} loop={true} style={{ width: '100%', height: '100%' }} />
+            </div>
         </div>
     )
 }
