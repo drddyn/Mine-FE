b2ca103e948390eb247a40fd95bfe8d083c526c6
yoonvinjeong <anaabc1004@naver.com>
2026-04-06 15:59:35 +0900

    fix: 사이드바 프로필 부분 스켈레톤 이상 해결

--- Mine/src/components/skeleton/SkeletonBase.tsx
+++ Mine/src/components/skeleton/SkeletonBase.tsx
@@ -4,14 +4,17 @@ interface SkeletonBaseProps {
     className?: string
 }
 
-export function SkeletonTitle({ className, children }: { children?: React.ReactNode } & SkeletonBaseProps) {
-    return <div className={`skeleton-shimmer bg-gray-200 w-35 h-3.5 ${className}`}>{children}</div>
+export function SkeletonTitle({
+    className = 'w-35 h-3.5',
+    children,
+}: { children?: React.ReactNode } & SkeletonBaseProps) {
+    return <div className={`skeleton-shimmer bg-gray-200 ${className}`}>{children}</div>
 }
 
 export function SkeletonBox({ className }: SkeletonBaseProps) {
     return <div className={`skeleton-shimmer overflow-hidden bg-gray-200 ${className}`} />
 }
 
-export function SkeletonAvatar({ className }: SkeletonBaseProps) {
-    return <div className={`skeleton-shimmer rounded-full bg-gray-200 w-12.5 h-12.5 ${className}`} />
+export function SkeletonAvatar({ className = 'w-12.5 h-12.5' }: SkeletonBaseProps) {
+    return <div className={`skeleton-shimmer rounded-full bg-gray-200  ${className}`} />
 }
