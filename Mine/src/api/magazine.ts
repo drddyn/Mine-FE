3471959c24d2059d9c19bdfa641e9019f4ab9db9
yoonvinjeong <anaabc1004@naver.com>
2026-04-06 14:15:33 +0900

    feat: 피드 조회 api staletime 설정 추가

--- Mine/src/api/magazine.ts
+++ Mine/src/api/magazine.ts
@@ -86,7 +86,7 @@ export const deleteParagraph = async ({ magazineId, sectionId, paragraphId }: De
     return res.data
 }
 
-export const getMagazineFeed = async ({ cursorId, limit = 10 }: FeedDto): Promise<ResponseFeed> => {
+export const getMagazineFeed = async ({ cursorId, limit = 15 }: FeedDto): Promise<ResponseFeed> => {
     const res = await axiosInstance.get(`/api/magazines/feed`, {
         params: { cursorId, limit },
     })
