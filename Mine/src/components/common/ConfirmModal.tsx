ea60d55f6b9f83719e521fa586f95ee7a7bf3636
yoonvinjeong <anaabc1004@naver.com>
2026-05-03 21:08:19 +0900

    feat: UI 작업

--- Mine/src/components/common/ConfirmModal.tsx
+++ Mine/src/components/common/ConfirmModal.tsx
@@ -20,7 +20,7 @@ export default function ConfirmModal({
     isLoading = false,
 }: ConfirmModalProps) {
     return (
-        <div className="fixed inset-0 bg-gray-600-op40 z-[999] flex items-center justify-center">
+        <div className="fixed inset-0 bg-gray-600-op40 z-999 flex items-center justify-center">
             <div className="relative z-10 w-120 h-53 rounded-2xl bg-gray-600-op70 px-8 py-8 flex flex-col justify-between shadow-[0_4px_8px_rgba(0,0,0,0.12),0_16px_32px_rgba(0,0,0,0.20)]">
                 <div>
                     <h2 className="mb-3 font-semibold24 text-gray-100">{title}</h2>
