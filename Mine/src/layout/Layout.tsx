838dece3f65acca5d88aa77f18a67929794f37e4
yoonvinjeong <anaabc1004@naver.com>
2026-05-25 23:42:39 +0900

    feat: api 404와 연결

--- Mine/src/layout/Layout.tsx
+++ Mine/src/layout/Layout.tsx
@@ -4,12 +4,15 @@ import { useAuthStore } from '../stores/auth'
 import useToastStore from '../stores/toast'
 import Toast from '../components/common/Toast'
 import { useEffect } from 'react'
+import { useErrorStore } from '../stores/error'
+import NotFoundPage from '../pages/NotFoundPage'
 
 const TOAST_DURATION = 3000
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     const { isLoggedIn } = useAuthStore()
     const { message, hideToast } = useToastStore()
+    const isNotFound = useErrorStore((state) => state.isNotFound)
 
     useEffect(() => {
         if (!message) return
@@ -20,7 +23,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="relative h-screen w-full overflow-hidden">
             {isLoggedIn && <Sidebar />}
-            <main className="h-full w-full overflow-y-auto duration-300">{children}</main>
+            {isNotFound && <NotFoundPage />}
+            {!isNotFound && <main className="h-full w-full overflow-y-auto duration-300">{children}</main>}
             {message && (
                 <div className="fixed bottom-37.25 left-1/2 -translate-x-1/2 z-9999">
                     <Toast message={message} />
