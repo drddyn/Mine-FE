65921a1d9f08ea816d91e05c40fcaa6fc6bfd48f
yoonvinjeong <anaabc1004@naver.com>
2026-05-26 02:59:22 +0900

    feat: 필터링 단어 추가 및 모달 연결

--- Mine/src/components/common/HarmfulKeywordModal.tsx
+++ Mine/src/components/common/HarmfulKeywordModal.tsx
@@ -5,10 +5,9 @@ export default function HarmfulKeywordModal({ onClose }: Props) {
     return (
         <div className="fixed inset-0 bg-gray-600-op40 z-999 flex items-center justify-center">
             <div className="relative z-10 w-120 h-53 rounded-2xl bg-gray-600-op70 px-8 py-8 flex flex-col justify-between shadow-[0_4px_8px_rgba(0,0,0,0.12),0_16px_32px_rgba(0,0,0,0.20)]">
-                <div>
-                    <h2 className="mb-3 font-semibold24 text-heart">유해 키워드가 감지되었습니다.</h2>
-
-                    <p className="font-regular20 text-gray-100 whitespace-pre-line">
+                <div className="flex flex-col gap-3">
+                    <h2 className="font-semibold24 text-heart">유해 키워드가 감지되었습니다.</h2>
+                    <p className="font-regular20 text-gray-100 leading-none">
                         3회 이상 감지될 경우 계정이 제한될 수 있습니다.
                     </p>
                 </div>
@@ -16,7 +15,7 @@ export default function HarmfulKeywordModal({ onClose }: Props) {
                 <div className="flex justify-end gap-2">
                     <button
                         onClick={onClose}
-                        className="flex items-center justify-center w-25 h-12 rounded-lg border border-gray-100 font-medium16 text-gray-100 hover:bg-heart"
+                        className=" w-25 h-12 rounded-lg font-medium16 text-gray-100 border border-gray-100 hover:border-transparent hover:bg-heart"
                     >
                         예
                     </button>
